2d007f0f661a350f6d3a870b71a40c06ea1a2fe7
tarang upadhyay <tarangupadhyay26@gmail.com>
2026-02-26 12:34:33 +0530

    cra & tailwind

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+   <div className="text-3xl font-bold text-red-500">
+  Netflix GPT
+</div>
+  )
+}
+
+export default App
